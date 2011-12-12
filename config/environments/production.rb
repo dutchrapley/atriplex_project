f2fa33579d02ef8380790fb0f23d7379f342ee9e
John Krueger <jtmkrueger@gmail.com>
2011-12-11 19:07:18 -0700

    connect sendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,15 @@ AtriplexProject::Application.configure do
   
   # devise specific settings
   config.action_mailer.default_url_options = { :host => 'atriplex.org' }
+  
+  # sendgrid settings
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
