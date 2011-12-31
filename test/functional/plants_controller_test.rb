6b1aeae9acff225ebbe57e6aafee90ac62a7e4e6
John Krueger <jtmkrueger@gmail.com>
2011-12-31 16:09:51 -0700

    tests. some user, mostly for plants

--- test/functional/plants_controller_test.rb
+++ test/functional/plants_controller_test.rb
@@ -12,40 +12,47 @@ class PlantsControllerTest < ActionController::TestCase
   end
 
   def test_new
+    sign_in users(:john)
     get :new
     assert_template 'new'
   end
 
   def test_create_invalid
+    sign_in users(:john)
     Plant.any_instance.stubs(:valid?).returns(false)
     post :create
     assert_template 'new'
   end
 
   def test_create_valid
+    sign_in users(:john)
     Plant.any_instance.stubs(:valid?).returns(true)
     post :create
     assert_redirected_to plant_url(assigns(:plant))
   end
 
   def test_edit
+    sign_in users(:john)
     get :edit, :id => Plant.first
     assert_template 'edit'
   end
 
   def test_update_invalid
+    sign_in users(:john)
     Plant.any_instance.stubs(:valid?).returns(false)
     put :update, :id => Plant.first
     assert_template 'edit'
   end
 
   def test_update_valid
+    sign_in users(:john)
     Plant.any_instance.stubs(:valid?).returns(true)
     put :update, :id => Plant.first
     assert_redirected_to plant_url(assigns(:plant))
   end
 
   def test_destroy
+    sign_in users(:john)
     plant = Plant.first
     delete :destroy, :id => plant
     assert_redirected_to plants_url
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert User.new.valid?
+
+  test "should allow valid email" do
+    assert_match /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/, "email@test.com"
+  end
+
+  test "should prevent invalid email" do
+    assert_no_match /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/, "email@test"
   end
 end
