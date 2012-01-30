a837575c9197bd843e338080595d1ecf263e0b7a
John Krueger <jtmkrueger@gmail.com>
2012-01-29 20:10:21 -0700

    validation tests for plants

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,7 @@
 class Plant < ActiveRecord::Base
   attr_accessible :common_name, :genus, :species, :zone, :light, :moisture, :soil_texture, :soil_ph, :form, :height, :width, :root_span, :lifespan, :growth_rate, :native, :edible, :medicinal, :nitrogen_fixer, :ground_cover, :dynamic_accumulator, :nectary, :cover_crop, :seed_stock_sources, :local_examples, :sources, :notes, :insects, :animals, :flavor_profile
+
+  validates :common_name, :presence => true, :uniqueness => true
+  validates :genus, :presence => true
+  validates :species, :presence => true
 end
--- test/unit/plant_test.rb
+++ test/unit/plant_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PlantTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Plant.new.valid?
+  fixtures :plants
+  def setup
+    @plant_one = plants(:one)
+    @plant_three = plants(:three)
+  end
+
+  def test_validates_presence_of_common_name
+    @plant_one.common_name = nil
+    assert !@plant_one.save
+  end
+
+  def test_validates_presence_of_genus
+    @plant_one.genus = nil
+    assert !@plant_one.save
+  end
+
+  def validates_presence_of_species
+    @plant_one.species = nil
+    assert !@plant_one.save
+  end
+
+  def validates_uniqueness_of_common_name
+    @plant_three.common_name = "Amaranth"
+    assert !@plant_three.save
   end
 end
